b6728dba42fe4969c38795a217c857208ce88989
Csanee <114819112+Csanee@users.noreply.github.com>
2024-04-08 21:53:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,258 @@
+const kerdesek=[
+    {
+        kerdes:"Szeretek társaságban lenni",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Jobban szeretek csoportban dolgozni, mint egyedül",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Mindig rend van az asztalomon/szobámban",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Nagyon széles baráti köröm van",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Nagy nyomás alatt is meg tudom őrizni a hidegvérem",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Könnyen ki tudom mutatni az érzéseimet",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Szerintem a művészet hülyeség",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Egy kis hiba miatt nem kérdőjelezem meg a képességeimet",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Inkább a tényekre hallgatok, mintsem az érzéseimre",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Sosem ment nekem a kreatív munka",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Általában sok alkoholt fogyasztok fesztiválokon, bulikon",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Nem érdekel, hogy ki mit gondol rólam",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Egy koncert vagy meccs számomra sokkal jobb kikapcsolódás, mint a szobában ülni egész nap",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Mindig én kezdem el a beszélgetéseket",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Csapatmunkában inkább irányítani szeretek másokat, minthogy  parancsokat kövessek",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    },
+    {
+        kerdes:"Nem változik hirtelen a hangulatom",
+        answers:[
+            { text:"Egyáltalán nem jellemző", pontok:1},
+            { text:"Nem nagyon jellemző", pontok:2},
+            { text:"Nem tudok dönteni", pontok:3},
+            { text:"Kicsit jellemző", pontok:4},
+            { text:"Nagyon jellemző", pontok:5},
+        ]
+    }
+    
+];
+
+const questionElement=document.getElementById("kerdes");
+const valaszGomb=document.getElementById("valaszok");
+const kovetkezoGomb=document.getElementById("kovetkezo");
+
+let jelenlegiKerdesIndex=0;
+let score=0;
+
+function kezdes()
+{
+    jelenlegiKerdesIndex=0;
+    score=0;
+    kovetkezoGomb.innerHTML="Next";
+    mutasd();
+}
+
+function mutasd() {
+
+    reset();
+    let jelenlegiKerdes = kerdesek[jelenlegiKerdesIndex];
+    let kerdesSzam = jelenlegiKerdesIndex + 1;
+    questionElement.innerHTML = kerdesSzam + ". " + jelenlegiKerdes.kerdes;
+
+    jelenlegiKerdes.answers.forEach(valasz => 
+    {
+        const button = document.createElement("button");
+        button.innerHTML = valasz.text;
+        button.classList.add("button");
+        valaszGomb.appendChild(button);
+        if (valasz.pontok) {
+            button.dataset.pontok = valasz.pontok;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function reset()
+{
+kovetkezoGomb.style.display="none";
+while(valaszGomb.firstChild)
+{
+valaszGomb.removeChild(valaszGomb.firstChild);
+}
+}
+
+function selectAnswer(e)
+{
+    const selectedButton = e.target;
+    const pontszam = parseInt(selectedButton.dataset.pontok);
+    score += pontszam;
+    selectedButton.classList.add("valasztott");
+    Array.from(valaszGomb.children).forEach(button=>
+        {
+            if(button.classList!="valasztott")button.disabled=true;
+        });
+
+    kovetkezoGomb.style.display = "block";
+}
+
+function eredmenyek()
+{
+    reset();
+    questionElement.innerHTML=`Az eredmeny ${score}`;
+    if(score>=16 && score<40)questionElement.innerHTML=`Az a személyiségtípus vagy, amelyiknek a tagjai visszahúzódott idealisták. Még a legelvetemültebb emberekben és a legtragikusabb eseményekben is keresik a jót és mindig javítani szeretnének a világon. Kívülről nyugodtnak, visszahúzódónak, sőt akár félénknek is tűnhetnek, pedig egyáltalán nem hiányzik belőlük a belső tűz és szenvedély. A népesség csupán 4 százalékát teszik ki, ezért sajnos sokan félreértik őket. Ha viszont sikerül hasonszőrű embereket találniuk, olyan harmonikus kapcsolatokra tehetnek szert, amelyek a boldogság és ihlet kiapadhatatlan forrásaivá válnak.`;
+    if(score>=41 && score<55)questionElement.innerHTML=`Az a személyiségtípus vagy, amelyiknek a tagjait az elvek vezérlik, nem pedig a logika, az izgalom vagy a haszonelvűség. Döntéseikben a becsület, szépség, erkölcs és erény játszik fontos szerepet: a Mediátorokat tiszta szándék vezérli, nem a saját hasznuk, vagy a büntetéstől való félelem. Büszkék ezekre a kvalitásaikra és tegyük hozzá, hogy joggal, de mivel nem mindenki érti meg, mi motiválja cselekedeteiket, könnyen elszigetelődhetnek.`;
+    if(score>=56 && score<70)questionElement.innerHTML=`Az a személyiségtípus vagy, amelyiknek a tagjai vérbeli vezetők szenvedélyesek és karizmatikusak. A népesség körülbelül 2 százalékát teszik ki, ők a politikusaink, edzőink és tanáraink. Arra ösztönöznek másokat, hogy a lehető legjobbat hozzák ki magukból, és tegyenek minél több jót a világban. Magától értetődő önbizalmukból fakadó befolyásuk révén a Főszereplők büszkén és boldogan igazgatják az embereket, arra sarkallva őket, hogy együttes erővel fejlesszék magukat és környezetüket.`;
+    if(score>=71 && score<81)questionElement.innerHTML=`Az a személyiségtípus vagy, amelyiknek a tagjai képesek a legváratlanabb pillanatban táncra perdülni vagy elkezdeni énekelni. A Szórakoztatókat könnyen elragadja a pillanat izgalma és azt szeretnék, ha mindenki másra is átragadna ez az érzés. Nincs még egy másik személyiségtípus, amelyik ennyi időt és energiát áldozna mások bátorítására és ilyen ellenállhatatlan stílusban.`;
+}
+
+function handleNextButton()
+{
+    jelenlegiKerdesIndex++;
+    if(jelenlegiKerdesIndex<kerdesek.length)
+    {
+        mutasd();
+    }else
+    {
+        eredmenyek();
+    }
+}
+
+kovetkezoGomb.addEventListener("click",() =>
+{
+    if(jelenlegiKerdesIndex<kerdesek.length)
+    {
+        handleNextButton();
+    }
+    else
+    {
+        kezdes();
+    }
+}
+);
+
+kezdes();
